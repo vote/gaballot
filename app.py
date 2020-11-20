c4a09e2372d83ce1e5967d5797726db2244a87cc
Ari Steinberg <arimsteinberg@gmail.com>
2020-11-20 10:02:07 -0800

    force exact match for last names
    this should reduce risk of DoS and also seems appropriate
    from a privacy perspective

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def search():
 
         records = (
             VoteRecord.query.filter(VoteRecord.first.like(f"{first}%"))
-            .filter(VoteRecord.last.like(f"{last}%"))
+            .filter(VoteRecord.last == last)
             .order_by(VoteRecord.last, VoteRecord.first)
             .limit(26)
             .all()
