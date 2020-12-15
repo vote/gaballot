9a4c614c3ba2facb31be57c7621791a811a138b2
Ben Weissmann <benweissmann@gmail.com>
2020-12-15 15:59:36 -0500

    Update copy

--- static/homepage.js
+++ static/homepage.js
@@ -1,54 +1,50 @@
-// document.getElementById("homepage-graph").innerText = JSON.stringify(
-//   window.HOMEPAGE_GRAPH_DATA
-// );
+(function () {
+  var rows = [["Days Before", "General", "Runoff"]];
+  var daysBeforeSeries = Object.keys(window.HOMEPAGE_GRAPH_DATA);
 
-var rows = [["Days Before", "General", "Runoff"]];
+  daysBeforeSeries.forEach(function (daysBefore) {
+    rows.push([
+      // use negative value -- c3 automatically sorts ascending
+      -daysBefore,
+      window.HOMEPAGE_GRAPH_DATA[daysBefore].total_general,
+      window.HOMEPAGE_GRAPH_DATA[daysBefore].total_special,
+    ]);
+  });
 
-const daysBeforeSeries = Object.keys(window.HOMEPAGE_GRAPH_DATA);
-daysBeforeSeries.sort().reverse();
-
-daysBeforeSeries.forEach(function (daysBefore) {
-  rows.push([
-    // use negative value -- c3 automatically sorts ascending
-    -daysBefore,
-    window.HOMEPAGE_GRAPH_DATA[daysBefore].total_general,
-    window.HOMEPAGE_GRAPH_DATA[daysBefore].total_special,
-  ]);
-});
-
-console.log(rows);
-
-var chart = c3.generate({
-  bindto: "#homepage-graph",
-  data: {
-    x: "Days Before",
-    rows: rows,
-  },
-  axis: {
-    x: {
-      label: "Days Before Election",
-      tick: {
-        format: function (x) {
-          return -x;
+  c3.generate({
+    bindto: "#homepage-graph",
+    data: {
+      x: "Days Before",
+      rows: rows,
+    },
+    axis: {
+      x: {
+        label: "Days Before Election",
+        tick: {
+          format: function (x) {
+            return -x;
+          },
+        },
+      },
+      y: {
+        label: "Votes Cast",
+        min: 0,
+        max: 4000000,
+      },
+    },
+    grid: {
+      y: {
+        lines: [
+          { value: 4015475, text: "4,015,475 Early Votes Cast In General" },
+        ],
+      },
+    },
+    tooltip: {
+      format: {
+        title: function (d) {
+          return -d + " days before";
         },
       },
     },
-    y: {
-      label: "Votes Cast",
-      min: 0,
-      max: 4000000,
-    },
-  },
-  grid: {
-    y: {
-      lines: [{ value: 4015475, text: "Early Votes Cast In General" }],
-    },
-  },
-  tooltip: {
-    format: {
-      title: function (d) {
-        return -d + " days before";
-      },
-    },
-  },
-});
+  });
+})();
